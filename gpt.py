1e2dc891873be00a5a9a97242e9c8b6e5002b1ad
Basile Ferry <b.ferry@quinten-france.com>
2023-03-16 14:37:23 +0100

    upgrade to gpt-3.5-turbo

--- gpt.py
+++ gpt.py
@@ -13,7 +13,7 @@ api_key = ""
 
 
 api_key = os.getenv("API_KEY") if not api_key else api_key
-if not api_key or api_key=="":
+if not api_key:
     raise Exception("Seems like no API key was provided or that it is empty")
 
 parser = argparse.ArgumentParser(description="A simple openai api interface to download dalle images generated from prompt")
@@ -21,7 +21,6 @@ parser = argparse.ArgumentParser(description="A simple openai api interface to d
 prompt = None
 if not sys.stdin.isatty():
     input_stream = sys.stdin
-    #prompt = " ".join([line for line in input_stream])
     prompt = input_stream.read().replace('\n', ' ')
 else:
     parser.add_argument("prompt", type=str, help="the prompt for image generation")
@@ -32,11 +31,11 @@ prompt = prompt if prompt else args.prompt
 openai.api_key = api_key
 
 response = openai.Completion.create(
-    engine="text-davinci-002",
+    engine="gpt-3.5-turbo",
     prompt=prompt,
     temperature=0.5,
-    max_tokens=128,
-    top_p=1,
+    max_tokens=2048,
+    top_p=1,    
     frequency_penalty=0,
     presence_penalty=0
 )
